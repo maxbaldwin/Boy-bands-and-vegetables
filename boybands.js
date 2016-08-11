cbf24b0b32a551efeca63f6ea3237a3d388149ef
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-11 18:55:06 -0500

    bathroom break

--- boybands.js
+++ boybands.js
@@ -11,17 +11,18 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
-
+  currentBand = bands[loopTracker];
+  var bandDiv = document.createElement("div"); 
+  var
 
   // Add the veggie names into the correct <div>
   currentVeggie = ???;
